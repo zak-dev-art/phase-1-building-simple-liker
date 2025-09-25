f7a69a0666f38553193800b54a4391fff0eef97d
zak-dev-art <zachndungu861@gmail.com>
2025-09-25 13:49:45 +0300

    modified html and js

--- main.js
+++ main.js
@@ -3,22 +3,45 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.getElementById("modal")
+  // Hide modal on page load
+  modal.classList.add("hidden")
 
+  const hearts = document.querySelectorAll(".like-glyph")
 
-
+  hearts.forEach(heart => {
+    heart.addEventListener("click", () => {
+      mimicServerCall()
+        .then(() => {
+          if (heart.textContent === EMPTY_HEART) {
+            heart.textContent = FULL_HEART
+            heart.classList.add("activated-heart")
+          } else {
+            heart.textContent = EMPTY_HEART
+            heart.classList.remove("activated-heart")
+          }
+        })
+        .catch(error => {
+          modal.classList.remove("hidden")
+          modal.querySelector("#modal-message").textContent = error
+          setTimeout(() => modal.classList.add("hidden"), 3000)
+        })
+    })
+  })
+})
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
-
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
-        reject("Random server error. Try again.");
+        reject("Random server error. Try again.")
       } else {
-        resolve("Pretend remote server notified of action!");
+        resolve("Pretend remote server notified of action!")
       }
     }, 300);
   });
